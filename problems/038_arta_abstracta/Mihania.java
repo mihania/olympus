2b20454b4880c78acd6aa0ab8bd64e6bddbae110
Mike Smacinih <msmacinih@snapchat.com>
2020-08-06 00:22:52 -0700

    038

--- problems/038_arta_abstracta/Mihania.java
+++ problems/038_arta_abstracta/Mihania.java
@@ -3,7 +3,13 @@ import java.util.*;
 import java.math.*;
 
 public class Mihania {
-    private static int solve(int[][] P) {
+
+    /**
+     * Time complexity: O(N^4)
+     * @param P
+     * @return
+     */
+    private static int solveN4(int[][] P) {
         int res = 0;
         for (int i = 0; i < P.length; i++) {
             for (int j = 0; j < P.length; j++) {
@@ -29,6 +35,49 @@ public class Mihania {
         return res / 8;
     }
 
+    /**
+     * Time complexity: O(N^2)
+     * @param P
+     * @return
+     */
+    private static int solveN2(int[][] P) {
+        Set<String> set = new HashSet<>();
+        for (int[] point : P) {
+            set.add(Arrays.toString(point));
+        }
+
+        int res = 0;
+        for (int i = 0; i < P.length; i++) {
+            for (int j = i + 1; j < P.length; j++) {
+
+                // P[i] and P[j] - are points of square diagonal, finding the antidiagonal points.
+
+                // checking that points color matching.
+                if (P[i][2] == P[j][2]) {
+
+                    // calculations are done based on https://www.quora.com/Given-two-diagonally-opposite-points-of-a-square-how-can-I-find-out-the-other-two-points-in-terms-of-the-coordinates-of-the-known-points
+                    int m1 = P[i][0] + P[j][0] + P[j][1] - P[i][1];
+                    int m2 = P[i][1] + P[j][1] + P[i][0] - P[j][0];
+                    int m3 = P[i][0] + P[j][0] + P[i][1] - P[j][1];
+                    int m4 = P[i][1] + P[j][1] + P[j][0] - P[i][0];
+                    if (m1 % 2 == 0 && m2 % 2 == 0 && m3 % 2 == 0 && m4 % 2 == 0) {
+                        int[] b = new int[]{m1 / 2, m2 / 2, P[i][2]};
+                        int[] d = new int[]{m3 / 2, m4 / 2, P[i][2]};
+                        if (set.contains(Arrays.toString(b)) && set.contains(Arrays.toString(d))) {
+                            res++;
+                        }
+
+                        System.out.printf("A=%s C=%s B=%s D=%s res=%d\n",
+                                Arrays.toString(P[i]), Arrays.toString(P[j]), Arrays.toString(b), Arrays.toString(d), res);
+                    }
+                }
+            }
+        }
+
+        // every square was detected twice, as every square has two diagonals
+        return res / 2;
+    }
+
     private static double dist(int[] a, int[] b) {
         return Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2);
     }
@@ -41,7 +90,7 @@ public class Mihania {
             for (int i = 0; i < tests.size(); i++) {
                 // System.out.printf("Starting test %d\n", i);
                 Test test = tests.get(i);
-                testOut.append(String.valueOf(solve(test.P)));
+                testOut.append(String.valueOf(solveN2(test.P)));
                 testOut.newLine();
             }
         } finally {
