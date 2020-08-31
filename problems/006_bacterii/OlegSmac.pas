219a439cd197f495e200997eb66f28b48c449a8a
OlegSmac <oleansmacinih@gmail.com>
2020-08-31 10:01:13 +0300

    006

---
+++ problems/006_bacterii/OlegSmac.pas
@@ -0,0 +1,22 @@
+﻿var
+  fin : text;
+  fout : text;
+  p, q, N, i, tmp : integer;
+begin
+  assign(fin,'BACTERII.IN');
+  reset(fin);
+  assign(fout,'BACTERII.OUT');
+  rewrite(fout);
+  read(fin, p);
+  read(fin, q);
+  read(fin, N);
+  for i := 0 to N-1 do
+  begin
+    tmp := p + q;
+    q := p;
+    p := tmp;
+  end;
+  write(fout, p + q);
+  close(fin);
+  close(fout);
+end.  
