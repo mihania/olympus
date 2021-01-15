b8de296120d2ffb2d5543f12c4c52fe2339d31fc
OlegSmac <oleansmacinih@gmail.com>
2021-01-15 20:33:14 +0200

    010

---
+++ problems/010_triplete_lui_pitagora/OlegSmac.pas
@@ -0,0 +1,44 @@
+﻿function NOD(a, b : integer) : integer;
+begin
+  while (a <> b) do 
+  begin
+    if (a > b) then
+    begin
+      a := a - b;
+    end;
+    if (b > a) then
+    begin
+      b := b - a;
+    end;  
+  end;
+  NOD := a;
+end;
+
+var 
+  fin, fout : text;
+  a, b, c, n : int64;
+begin
+  assign(fin, 'TRIP.IN');
+  reset(fin);
+  assign(fout, 'TRIP.OUT');
+  rewrite(fout);
+  read(fin, c);
+  for a := 1 to c - 1 do
+  begin
+    b := round(sqrt(sqr(c) - sqr(a)));
+    if (sqr(a) + sqr(b) = sqr(c)) and (a < b) then
+    begin
+      if NOD(a, b) = 1 then
+      begin
+        writeln(fout, a, ' ', b, ' ', c);
+        n := n + 1;  
+      end;
+    end;
+  end;
+  if (n = 0) then
+  begin
+    writeln(fout, 'NOPE');
+  end;
+  close(fin);
+  close(fout);
+end.
