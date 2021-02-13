de9bbe4b94a4496ffc41181a5d540484bf656cb7
Mihail Smacinih <mihania@users.noreply.github.com>
2021-02-13 15:40:25 -0800

    Update Mihania.java

--- problems/011_submultimi/Mihania.java
+++ problems/011_submultimi/Mihania.java
@@ -3,19 +3,18 @@ public class Main {
         int N = 64;
         int K = 7;
         
-        // inspiration from https://www.geeksforgeeks.org/count-of-subsets-of-integers-from-1-to-n-having-no-adjacent-elements/
-        
         // dp[i][j][0] - number of desirable subsets ending at index i of size j without element 1
         // dp[i][j][1] - number of desirable subsets ending at index i of size j with element 1
         long[][][] dp = new long[N + 1][K + 1][2];  
                 
-        for (int i = 1; i < dp.length; i++) {
+        for (int i = 1; i <= N; i++) {
+            
             // adding self element {i}
             dp[i][1][i == 1 ? 1 : 0] = 1;     
             
             // traversing up to i - 2 to exclude adjancent element (i - 1), adjacent to i
             for (int k = 1; k < i - 1; k++) {                
-                for (int j = 2; j < dp[0].length; j++) {
+                for (int j = 2; j <= K; j++) {
                     dp[i][j][0] += dp[k][j - 1][0];
                     dp[i][j][1] += dp[k][j - 1][1];
                 }
@@ -23,11 +22,11 @@ public class Main {
         }
         
         int res = 0;
-        for (int i = 1; i < dp.length; i++) {
+        for (int i = 1; i <= N; i++) {
             res += dp[i][K][0];
             
-            // preventing N and 1 to be adjacent.
-            if (i != dp.length - 1) {
+            // prenting N and 1 to be adjacent.
+            if (i != N) {
                 res += dp[i][K][1];
             }
         }
