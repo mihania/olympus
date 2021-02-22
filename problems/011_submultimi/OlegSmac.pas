30b109d124f8277c37d4006a927e68c65c333ba2
OlegSmac <oleansmacinih@gmail.com>
2021-02-22 23:28:14 +0200

    011

--- problems/011_submultimi/OlegSmac.pas
+++ problems/011_submultimi/OlegSmac.pas
@@ -1,75 +1,51 @@
-﻿function IsValid(subset : array of integer; k : integer) : boolean;
-var i, len, prev : integer;
-    res : boolean;
+﻿function Solve(n, k : integer) : integer;
+var a : array [1..444, 1..444, 0..1] of integer;
+    i, j, x, sum1, sum0, res_sum : integer;
 begin
-  res := TRUE;
-  for i := 0 to length(subset) - 1 do
+  if (k = 1) and (n = 1) then
   begin
-    if (subset[i] <> 0) then
-    begin
-      if (i = 0) then
-      begin
-        prev := length(subset) - 1;
-      end
-      else begin
-        prev := i - 1;  
-      end;
-      len := len + 1;
-      if ((subset[prev] <> 0) and (length(subset) <> 1)) then
-      begin
-        res := FALSE;
-        break;
-      end;
-    end;
-  end;
-  IsValid := res and (len = k);
-end;
-
-function Solve(n, k : integer) : integer;
-var subset : array of integer;
-    i, j, x, p, tmp, num : integer;
-begin
-  setLength(subset, n);
-  tmp := 1;
-  for i := 0 to n - 1 do //math.pow(2, time.length);
-  begin
-    tmp := tmp * 2;
+    res_sum := 1;
   end;
   
-  for x := 0 to tmp - 1 do //создание подмножеств
+  for i := 1 to n do //длина 1
   begin
-    p := x;
-    i := 0;
-    while (p > 0) do
+    a[1][i][1] := 1;
+    if (i <> 1) then a[1][i][0] := 1;
+  end;
+  
+  for i := 2 to n do
+  begin
+    for j := 1 to n do
     begin
-      if (p mod 2 = 1) then
+      for x := 1 to j - 2 do
       begin
-        subset[i] := i + 1;
+        sum1 := sum1 + a[i - 1][x][1];
+        sum0 := sum0 + a[i - 1][x][0];
       end;
-      p := p div 2;
-      i := i + 1;
-    end;
-    if IsValid(subset, k) then
-    begin
-      num := num + 1;  
-    end;
-    
-    for j := 0 to length(subset) - 1 do //обнуление массива
-    begin
-      subset[j] := 0;
+      a[i][j][1] := sum1;
+      a[i][j][0] := sum0;
+      sum1 := 0;
+      sum0 := 0;
     end;
   end;
-  Solve := num;
+  
+  for i := 1 to n - 1 do
+  begin
+    res_sum := res_sum + a[k][i][1];
+  end;
+  res_sum := res_sum + a[k][n][0];
+  Solve := res_sum;
 end;
 
 var fin, fout : text;
-    n, k, T, i : integer;
+    T, n, k, i : integer;
 begin
   assign(fin, 'TESTS.IN');
   reset(fin);
   assign(fout, 'TESTS.OUT');
   rewrite(fout);
   readln(fin, T);
+  //writeln(Solve(1, 1));
   for i := 0 to T - 1 do
   begin
     read(fin, n);
@@ -77,5 +53,5 @@ begin
     writeln(fout, Solve(n, k));
   end;
   close(fin);
-  close(fout);
+  close(fout);  
 end.
