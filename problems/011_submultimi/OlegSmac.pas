daeaeade6561bd9457522424eee4f0693c5ea934
OlegSmac <oleansmacinih@gmail.com>
2021-02-23 21:35:44 +0200

    011

--- problems/011_submultimi/OlegSmac.pas
+++ problems/011_submultimi/OlegSmac.pas
@@ -1,5 +1,10 @@
-﻿function Solve(n, k : integer) : integer;
-var a : array [1..444, 1..444, 0..1] of integer;
+﻿type BigInteger = record
+     digits : array [0..259] of byte;
+     size : integer;
+end;
+
+function Solve(n, k : integer) : integer;
+var dp : array [1..444, 1..444, 0..1] of integer;
     i, j, x, sum1, sum0, res_sum : integer;
 begin
   if (k = 1) and (n = 1) then
@@ -9,31 +14,27 @@ begin
   
   for i := 1 to n do //длина 1
   begin
-    a[1][i][1] := 1;
-    if (i <> 1) then a[1][i][0] := 1;
+    dp[1][i][1] := 1;
+    if (i <> 1) then dp[1][i][0] := 1;
   end;
   
-  for i := 2 to n do
+  for i := 2 to k do
   begin
     for j := 1 to n do
     begin
       for x := 1 to j - 2 do
       begin
-        sum1 := sum1 + a[i - 1][x][1];
-        sum0 := sum0 + a[i - 1][x][0];
+        dp[i][j][1] := dp[i][j][1] + dp[i - 1][x][1]; //сумма всех подмножеств, заканчивающихся на i элемент
+        dp[i][j][0] := dp[i][j][0] + dp[i - 1][x][0]; //сумма подмножеств, в которых нет цифры 1 и которые заканчиваются на i элемент
       end;
-      a[i][j][1] := sum1;
-      a[i][j][0] := sum0;
-      sum1 := 0;
-      sum0 := 0;
     end;
   end;
   
   for i := 1 to n - 1 do
   begin
-    res_sum := res_sum + a[k][i][1];
+    res_sum := res_sum + dp[k][i][1];
   end;
-  res_sum := res_sum + a[k][n][0];
+  res_sum := res_sum + dp[k][n][0];
   Solve := res_sum;
 end;
 
@@ -45,7 +46,6 @@ begin
   assign(fout, 'TESTS.OUT');
   rewrite(fout);
   readln(fin, T);
-  //writeln(Solve(1, 1));
   for i := 0 to T - 1 do
   begin
     read(fin, n);
