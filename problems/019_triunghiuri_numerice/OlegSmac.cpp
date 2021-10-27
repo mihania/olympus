3c65654d4f9ae0b82104d449d8c73435fb2e5935
OlegSmac <oleansmacinih@gmail.com>
2021-10-27 22:24:59 +0300

    19

--- problems/019_triunghiuri_numerice/OlegSmac.cpp
+++ problems/019_triunghiuri_numerice/OlegSmac.cpp
@@ -6,8 +6,11 @@ using namespace std;
 
 int minim = -1;
 int maxim;
+vector<vector<int>> nums;
+int n;
 
-void findPaths(int n, vector<vector<int>> nums, int path, int levelI, int levelJ) {
+void findPaths(int path, int levelI, int levelJ) {
+	cout << "path = " << path << " levI = " << levelI << " levJ = " << levelJ << endl;
 	if (levelI == n) {
 		if (minim == -1 || path < minim) {
 			minim = path;
@@ -16,29 +19,21 @@ void findPaths(int n, vector<vector<int>> nums, int path, int levelI, int levelJ
 			maxim = path;
 		}
 	}
-	for (int i = levelI; i < n; i++) {
-		for (int j = levelJ; j <= levelJ + 1; j++) {
-			cout << "i = " << i << " j = " << j << " nums[i][j] = " << nums[i][j];
-			cout << " levelI = " << levelI << " levelJ = " << levelJ << " path = " << path << endl;
-			findPaths(n, nums, path + nums[i][j], i + 1, j);
+	else {
+		findPaths(path + nums[levelI][levelJ], levelI + 1, levelJ);
+		if (levelJ + 1 <= levelI) {
+			findPaths(path + nums[levelI][levelJ + 1], levelI + 1, levelJ + 1);
 		}
-		cout << endl;
-		break;
 	}
 }
 
 int main() {
 	ifstream in("in8.txt");
-	int n;
+	ofstream out("res.txt");
 	in >> n;
 	if (n == 0) {
-		return 0;
+		out << 0 << " " << 0; 
 	}
-	if (n == 1) {
-		int num; in >> num;
-		return num;
-	}
-	vector<vector<int>> nums;
 	for (int i = 0; i < n; i++) {
 		vector<int> level;
 		for (int j = 0; j < i + 1; j++) {
@@ -48,15 +43,21 @@ int main() {
 		}
 		nums.push_back(level);
 	}
-	
-	findPaths(n, nums, nums[0][0], 1, 0);
+	findPaths(0, 0, 0);
 	cout << "min = " << minim << endl;
 	cout << "max = " << maxim << endl;
 	
-	ofstream out("res.txt");
 	out << minim << " " << maxim;
 	in.close();
 	out.close();
 	
 	cout << "Program is done.\n";
 }
+
+/*
+for (int j = levelJ; j <= levelJ + 1 && j <= levelI; j++) {
+		//cout << "i = " << i << " j = " << j << " nums[i][j] = " << nums[i][j];
+		cout << " levelI = " << levelI << " levelJ = " << levelJ << " path = " << path << endl;
+		findPaths(path + nums[levelI][j], levelI + 1, j);
+	}
+*/
