f25670d83c6816c572bd7738e36100fca42647c5
Mike Smacinih <mihania@gmail.com>
2021-10-29 18:39:56 -0700

    comments

--- problems/020_cifra_de_control_2/mihania.cpp
+++ problems/020_cifra_de_control_2/mihania.cpp
@@ -4,6 +4,13 @@
 using namespace std;
 
 int main() {
+
+	// let A = a0 + 10a1 + 10^2 * a2 + ... 
+	// A % 9 = (a0 + (9 + 1)a1 + (99 + 1)a2 +...) % 9 = (a0 + a1 + a2+ ..) % 9
+	// => A % 9 = (a0 + a1 + a2 + .. aN) %9
+	// let dp[n][k] = count of numbers of length n which control sum is k
+	// dp[n][k] = dp[n - 1][k] * 10
+	// => M = 10^(n - 1)
 	ifstream in("in.txt");
 	long n;
 	in >> n;
