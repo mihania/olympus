e555b96432afe1a24722ce6c6fb0b2610d04225c
OlegSmac <oleansmacinih@gmail.com>
2021-11-01 19:43:57 +0200

    20

--- problems/020_cifra_de_control_2/OlegSmac.cpp
+++ problems/020_cifra_de_control_2/OlegSmac.cpp
@@ -1,43 +1,29 @@
 #include<iostream>
 #include<fstream>
 #include<math.h>
+#include<array>
 
 using namespace std;
 
-int findSumDigits(long num) {
-	if (num / 10 == 0) return num;
-	//cout << "num = " << num;
-	long sum = 0;
-	while (num != 0) {
-		sum += num % 10;
-		num = num / 10;
-	}
-	//cout << " sum = " << sum << endl;
-	return findSumDigits(sum);
-} 
-
-int myPow(long n) {
-	long res = 10;
-	for (int i = 2; i <= n; i++) {
-		res = res * 10;
-	}
-	return res;
-}
-
 int main() {
-	ifstream in("in23.txt");
+	ifstream in("in45.txt");
 	long n;
 	int k;
-	int M = 0;
+	string M;
 	in >> n;
 	in >> k;
-	long start = myPow(n - 1);
-	long end = myPow(n); 
-	//cout << "start = " << start << " end = " << end << endl;
-	for (long i = start; i < end; i++) {
-		if (findSumDigits(i) == k) {
-			M++;
-		}
+	//375 = 15 = 6
+	//an, a(n - 1) .. a2, a1, a0;
+	//(a0 + 10 * a1 + 100 * a2 + .. 10^n * an) % 9 = (a0 + a1 + .. + an) % 9;
+	//a0 + (1 + 9) * a1 + (1 + 99) * a2 ...
+	//(a0 + a1 + 9a1 + a2 + 99a2 ...) % 9
+	//(a0 + a1 + 0 + a2 + 0 ...) % 9
+	//array dp[n + 1][10] (int)
+	//dp[i][j] - количество чисел, длиной i и остатком деления на 9, равное j. 
+	//dp[n][k] - решение
+	M = "1";
+	for (int i = 0; i < n - 1; i++) {
+		M += "0";
 	}
 	cout << "M = " << M << endl;
 	ofstream out("res.txt");
