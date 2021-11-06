b478aa213836e604bd707c0d3296e7c71091822d
Mike Smacinih <mihania@gmail.com>
2021-11-05 23:42:18 -0700

    improve comments

--- problems/021_enigma_2/mihania.cpp
+++ problems/021_enigma_2/mihania.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 // Let's assume A length is N, B length is M.
 // Let's divide string B into three parts [left|middle|right], where both left and right are of length N.
-// Each letter c in middle part can be crossed T times where T - is the number of occurences of letter c in A.
+
+// To simplify the task, let's assume we need to find number of intersections of each B' in middle.
+// We can reverse the problem and see that for each letter c in middle we can intersect it T times, 
+// where T - is the number of occurences of letter c in A, because we can shift B' to this required position.
+
 // For the left and right part logic is the same, but corner cases must be applied for calculating histogram of each letter. 
 int main() {
 	ifstream in("in.txt");
