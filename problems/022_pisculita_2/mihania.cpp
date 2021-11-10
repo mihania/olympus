11a9bb8176a57fa45122ae03ff5cd1294f213416
Mike Smacinih <mihania@gmail.com>
2021-11-10 12:36:59 -0800

    rename

--- problems/022_pisculita_2/mihania.cpp
+++ problems/022_pisculita_2/mihania.cpp
@@ -10,36 +10,36 @@ int main() {
 	in >> N >> G;
 	in.close();		
 
-	// c[i][j] - number of combinations of i coins with weight j
-        vector<vector<long long>> c(N + 1, vector<long long>(G + 1, 0));	
+	// k[i][j] - number of combinations of i coins with weight j
+        vector<vector<long long>> k(N + 1, vector<long long>(G + 1, 0));	
 	
-	// mc[i][j] - minimum value of i coins with weight j 
-	vector<vector<long long>> mc(N + 1, vector<long long>(G + 1, LONG_MAX)); 
+	// m[i][j] - minimum value of i coins with weight j 
+	vector<vector<long long>> m(N + 1, vector<long long>(G + 1, LONG_MAX)); 
 
 	// coin weight -> value mapping
 	vector<int> coins = {0, 1, 5, 10, 25, 50};
 
-	for (int j = 1; j < min(c[0].size(), coins.size()); j++) {
-		c[1][j] = 1;
-		mc[1][j] = coins[j];
+	for (int j = 1; j < min(k[0].size(), coins.size()); j++) {
+		k[1][j] = 1;
+		m[1][j] = coins[j];
 	}
 	
-	for (int i = 1; i < c.size(); i++) {
-		for (int j = 1; j < c[0].size(); j++) {
-			for (int k = 1; k < min(j + 1, (int)coins.size()); k++) {
+	for (int i = 1; i < k.size(); i++) {
+		for (int j = 1; j < k[0].size(); j++) {
+			for (int t = 1; t < min(j + 1, (int)coins.size()); t++) {
 				
 				// checking that a combination exist
 				// preventing duplicate combinations
-				if (c[i - 1][j - k] > 0 && j - k >= k) {
-					c[i][j] += c[i - 1][j - k];
-					mc[i][j] = min(mc[i][j], mc[i - 1][j - k] + coins[k]);
+				if (k[i - 1][j - t] > 0 && j - t >= t) {
+					k[i][j] += k[i - 1][j - t];
+					m[i][j] = min(m[i][j], m[i - 1][j - t] + coins[t]);
 				}
 			}
 		}
  	}
 
 	ofstream out("out.txt");
-	out << (c[N][G] == 0 ? 0 : mc[N][G]) << " " << c[N][G] << endl;
+	out << (k[N][G] == 0 ? 0 : m[N][G]) << " " << k[N][G] << endl;
 	out.close();
 	return 0;
 }
