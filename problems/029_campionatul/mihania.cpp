5d3aa7a8f82fa9d837961eadf5b63d8cb4388684
Mike Smacinih <mihania@gmail.com>
2021-12-01 21:52:28 -0800

    100%

--- problems/029_campionatul/mihania.cpp
+++ problems/029_campionatul/mihania.cpp
@@ -1,44 +1,53 @@
 #include<fstream>
 #include<vector>
-#include<map>
 #include<numeric>
-#include<set>
 using namespace std;
 
-long long solve(const long long N, long long S, vector<long long>& c2) {
-	vector<long long> c;
-	
-	// filter out elements greater than S
-	copy_if(c2.begin(), c2.end(), back_inserter(c), [S](long i) { return i <= S; });
-	
-	// S cannot be bigger than sum of tickets
-	S = min(S, accumulate(c.begin(), c.end(), (long long)0));
-	
-	map<long long, long long> m;
-    	m[0] = 1;
-    	for (auto k : c) {
-		vector<long long> values;
-	       	long long prev = -1;
-		for (auto it = m.rbegin(); it != m.rend(); it++) {
-			
-			// inserting a new element in the map changes the iterator order. dirty fix to not go 
-			// over the same element again.
-			auto i = it->first;
-			if (prev == i) {
-				continue;
+vector<long long> getSums(long long S, vector<long long>& c) {
+	vector<long long> res;
+	long long max = (long long)pow(2, c.size());
+	for (long long k = 0; k < max; k++) {
+		long long sum = 0;
+		int idx = 0;
+		auto mask = k;
+		while (mask > 0) {
+			if (mask % 2 == 1) {
+				sum += c[idx];
 			}
 
-			prev = i;
-			
-			if (i + k <= S) {
-			 	m[i + k] += m[i];	
-			}
-		}	
-    	}
+			mask /= 2;
+			idx++;
+		}
 
-    	long long res = 0;
-	for (auto it : m) {
-		res += it.second;
+		if (sum <= S) {
+			res.push_back(sum);
+		}
+
+	}
+
+	return res;
+}
+
+long long solve(long long S, vector<long long>& c) {
+	
+	// splitting array into two equal subarrays
+	auto v1 = vector<long long>(c.begin(), c.begin() + c.size() / 2);
+	auto v2 = vector<long long>(c.begin() + c.size() / 2, c.end());
+
+	// calculating all possible subset sums in each array
+	auto s1 = getSums(S, v1);
+	auto s2 = getSums(S, v2);
+	
+	sort(s2.begin(), s2.end());
+	
+	// erasing empty set from the second array
+	s2.erase(s2.begin());
+
+	// merging arrays subset sums
+	long long res = 0;
+	for (auto k : s1) {
+		auto it = upper_bound(s2.begin(), s2.end(), S - k);
+		res += 1 + (it - s2.begin());
 	}
 
 	return res;
@@ -62,10 +71,7 @@ int main() {
 			c.push_back(v);
 		}
 		
-		printf("i=%d N=%lld S=%lld\n", i, N, S);
-		long long res = solve(N, S, c);
-		out << res << endl;
-		printf(" res=%lld\n", res);
+		out << solve(S, c) << endl;
 	}
 
 
@@ -73,3 +79,4 @@ int main() {
 	out.close();
 	return 0;
 }
+
