32b45055dcbc453a8181063030aae4e30adb43f0
Mike Smacinih <mihania@gmail.com>
2021-12-15 22:42:11 -0800

    draft

--- problems/033_postasul/mihania.cpp
+++ problems/033_postasul/mihania.cpp
@@ -30,6 +30,83 @@ void printf(vector<vector<T>>& m) {
 	printf("\n");
 }
 
+bool canReach(Test& t, long houseCount, long minH, long maxH) {
+	vector<vector<bool>> visited(t.N, vector<bool>(t.N, false));
+	
+	if (t.z[t.P.first][t.P.second] < minH || t.z[t.P.first][t.P.second] > maxH) {
+		return false;
+	}	
+
+	queue<tuple<int, int>> q;
+	q.push({t.P.first, t.P.second});
+	visited[t.P.first][t.P.second] = true;
+	int count = 0;
+	while (!q.empty() && count < houseCount) {
+		int i = get<0>(q.front());
+		int j = get<1>(q.front());
+		q.pop();
+		
+		for (auto& dpp : dppp) {
+			int ni = i + dpp[0];
+			int nj = j + dpp[1];
+			if (ni >= 0 && ni < t.N && nj >= 0 && nj < t.N
+					&& !visited[ni][nj] 
+					&& t.z[ni][nj] >= minH && t.z[ni][nj] <= maxH) {
+				
+				visited[ni][nj] = true;
+				q.push({ni, nj});
+				if (t.h[ni][nj] == 'K') {
+					count++;
+				}
+			}	
+		}
+	}
+
+	return count == houseCount;
+}
+
+bool canReach(Test& t, long houseCount, long interval, long minH, long maxH) {
+	for (int i = minH; i + interval <= maxH; i++) {
+		if (canReach(t, houseCount, i, i + interval)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+long solveV2(Test& t) {
+	// getting some statistics
+	auto maxH = 0; 		// the max height of all cells
+	auot minH = LONG_MAX;	// the min height of all cells
+	long count = 0; 	// number of houses.
+	for (auto i = 0; i < t.h.size(); i++) {
+		for (auto j = 0; j < t.h.size(); j++) {
+			if (t.z[i][j] == 'K') {
+				count++;
+			}
+			
+			minH = min(minH, t.h[i][j]);
+			maxH = max(maxH, t.h[i][j]);
+		}
+	}
+
+
+	// binary searh on min interval size that is enough to get to all houses.
+	auto i = 0;
+	auto j = maxH;
+	while (i < j) {
+		auto mid = i + (j - i) / 2;
+		if (canReach(t, count, mid, minH, maxH)) {
+			j = mid;
+		} else {
+			i = mid + 1;
+		}
+	}
+
+	return j;
+}
+
 
 long solve(Test& t) {
 	
@@ -120,7 +197,7 @@ int main() {
 
 //		if (t == 1) 
 		{
-			out << solve(test) << endl;
+			out << solveV2(test) << endl;
 		}
 	}
 
