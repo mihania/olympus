732b7794dc7db17fdba0030c749dd38cb89532ca
Nikitasm <68600537+NikitaSmac@users.noreply.github.com>
2022-01-15 16:11:35 +0200

    Update Nick_005.cpp

--- problems/005_focuri_de_artificii/Nick_005.cpp
+++ problems/005_focuri_de_artificii/Nick_005.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 using namespace std;
 int main(){
-    int n, in_n;
-    cin >> n;
-    vector<int> h;
-    vector<int> a;
-    vector<int> b;
-    int in_h, in_a, in_b;
-    for(int i = 0; i < n; i++){
-        cin >> in_h;
-        h.push_back(in_h);
-        cin >> in_a;
-        a.push_back(in_a);
-        cin >> in_b;
-        b.push_back(in_b);
-    }
-    int x = 1;
-    int max_x = 0;
-    for(int i = 1; i < n; i++){
-        if((h[i] >= a[i  - 1]) && (h[i] <= b[i - 1])){
-                x++;
+    ifstream in("Test.txt");
+    ofstream out("res.txt");
+    int T;
+    in >> T;
+    for(int i = 0; i < T; i++){
+        int n, in_n;
+        cin >> n;
+        vector<int> h;
+        vector<int> a;
+        vector<int> b;
+        int in_h, in_a, in_b;
+        for(int i = 0; i < n; i++){
+            cin >> in_h;
+            h.push_back(in_h);
+            cin >> in_a;
+            a.push_back(in_a);
+            cin >> in_b;
+            b.push_back(in_b);
         }
-        else {
-            if(max_x <= x){
-                max_x = x;
+        int x = 1;
+        int max_x = 0;
+        for(int i = 1; i < n; i++){
+            if((h[i] >= a[i  - 1]) && (h[i] <= b[i - 1])){
+                    x++;
             }
-            x = 0;
+            else {
+                if(max_x <= x){
+                    max_x = x;
+                }
+                x = 0;
+            }
         }
+        out << max_x;
     }
-    cout << max_x;
+    in.close();
+    out.close();
 }
