e61c8c7d23e47daff424f32a4303317ea6f45375
Nikitasm <68600537+NikitaSmac@users.noreply.github.com>
2022-01-20 22:29:16 +0200

    Update Nick_55.cpp

--- computer_science/055_numer_sum/Nick_55.cpp
+++ computer_science/055_numer_sum/Nick_55.cpp
@@ -2,36 +2,44 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <fstream>
 using namespace std;
 int main(){
-    int a, number;
-    cin >> a;
-    vector<int> test;
-    for(int i = 0; i < a; i++){
-        cin >> number;
-        test.push_back(number);
-    }
-    int sum, j, x, st;
-    sum = 0;
-    j = 0;
-    vector <int> answer;
-    for(int i = 0; i < a; i++){
-        x = test[i];
-        while(x > 9){
-            sum += x % 10;
-            x = x / 10;
-            j++;
+    ifstream("test.txt");
+    ofstream("res.txt");
+    int T;
+    in >> T;
+    for(int i = 0; i < T; i++){
+        int a, number;
+        in >> a;
+        vector<int> test;
+        for(int i = 0; i < a; i++){
+            in >> number;
+            test.push_back(number);
         }
-        st = pow(10, j);
-        if ((test[i] / st) == sum){
-            answer.push_back(test[i]);
-        }
-        j = 0;
+        int sum, j, x, st;
         sum = 0;
+        j = 0;
+        vector <int> answer;
+        for(int i = 0; i < a; i++){
+            x = test[i];
+            while(x > 9){
+                sum += x % 10;
+                x = x / 10;
+                j++;
+            }
+            st = pow(10, j);
+            if ((test[i] / st) == sum){
+                answer.push_back(test[i]);
+            }
+            j = 0;
+            sum = 0;
+        }
+        sort(answer.begin(), answer.end());
+        for(int i = answer.size() - 1; i >= 0; i--){
+            out << answer[i] << " ";
+        }
     }
-    sort(answer.begin(), answer.end());
-    for(int i = answer.size() - 1; i >= 0; i--){
-        cout << answer[i] << " ";
-    }
-
+    in.close();
+    of.close();
 }
