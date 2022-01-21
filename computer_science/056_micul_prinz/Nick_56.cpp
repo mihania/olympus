8c97b6fa20ad2b5987b02e06eb3d648385a78ccc
Nikitasm <68600537+NikitaSmac@users.noreply.github.com>
2022-01-21 16:22:31 +0200

    Update Nick_56.cpp

--- computer_science/056_micul_prinz/Nick_56.cpp
+++ computer_science/056_micul_prinz/Nick_56.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include<cmath>
+#include <cmath>
+#include <fstream>
 using namespace std;
 int main(){
-    int n, number;
-    cin >> n;
-    vector<int> test;
-    for(int i = 0; i < n; i++){
-        cin >> number;
-        test.push_back(number);
-    }
-    for(int i = 0; i < n; i++){
-        string str = to_string(test[i]);
-        int k = str.size() - 1;
-        for(int j = 0; j < str.size() / 2; j++){
-            while(str[j] != str[k]){
-                test[i] += pow(10, j);
-                str = to_string(test[i]);
-            }
-            k--;
+    ifstream in("test_56.txt");
+    ofstream out("res.txt");
+    in >> T;
+    for(int i = 0; i < T; i++){
+        int n, number;
+        cin >> n;
+        vector<int> test;
+        for(int i = 0; i < n; i++){
+            cin >> number;
+            test.push_back(number);
         }
-    }
-    for(int i = 0; i < n; i++){
-        cout << test[i] << " ";
+        for(int i = 0; i < n; i++){
+            string str = to_string(test[i]);
+            int k = str.size() - 1;
+            for(int j = 0; j < str.size() / 2; j++){
+                while(str[j] != str[k]){
+                    test[i] += pow(10, j);
+                    str = to_string(test[i]);
+                }
+                k--;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            cout << test[i] << " ";
+        }
+        in.close();
+        out.close();
     }
 }
