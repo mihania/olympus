4137200e22dd32db373f83cb29bb78b2509a7dfb
Nikitasm <68600537+NikitaSmac@users.noreply.github.com>
2022-01-21 16:19:08 +0200

    Update Nick_005.cpp

--- computer_science/005_Focuri_de_artificii/Nick_005.cpp
+++ computer_science/005_Focuri_de_artificii/Nick_005.cpp
@@ -27,7 +27,7 @@ int main(){
         int ans = 1;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < i; j++){
-                if((a[j] <= h[i]) && (h[i] >= b[j])){
+                if((a[j] <= h[i]) && (h[i] <= b[j])){
                     dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
