5bfd050bc4662feb9802a0edebef9e1b5c070dfa
Mike Smacinih <mihania@gmail.com>
2022-02-02 09:31:45 -0800

    O(N)

--- computer_science/062_Text/mihania.cpp
+++ computer_science/062_Text/mihania.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
 
@@ -17,28 +18,42 @@ int main() {
 	ofstream out("tests.out");
 	out << m.size() << endl;
 
-	// dp[i] - max length of subsequence number starting with word i
-	vector<int> dp(m.size(), 1);
-
 	// next[i] - the index of previous word of max subsequence if it starts with word i
 	vector<int> next(m.size(), -1);
 
+	// dp[i] - the max lenth of subsequence if sentense starts at word i.
+	vector<int> dp(m.size(), 1);
+
+	// mp[ch] - returns the best index in word list that starts with letter ch
+	unordered_map<char, int> mp;
+	
 	// searching LIS (longest increasing subsequence)
 	int maxI = 0;
 	for (int i = m.size() - 1; i >= 0; i--) {
-		for (int j = i + 1; j < m.size(); j++) {
-			if (	// does the word start with same letter?
-				m[i][m[i].length() - 1] == m[j][0]
-				
-				// is subsequence bigger?
-				&& dp[j] + 1 > dp[i]) {
+		
+		// first letter of the word
+		char fst = m[i][0];
 
+		// last letter of the word.
+		char lst = m[i][m[i].length() - 1];
+
+		if (mp.find(fst) == mp.end()) {
+			mp[fst] = i;
+		}
+		
+		if (	// does the word exist?
+			mp.find(lst) != mp.end()) {
+			
+			int j = mp[lst];
+
+			if (dp[i] < dp[j] + 1) {
 				dp[i] = dp[j] + 1;
 				next[i] = j;
+				mp[fst] = i;
+			}
 
-				if (dp[maxI] < dp[i]) {
-					maxI = i;
-				}
+			if (dp[maxI] < dp[i]) {
+				maxI = i;
 			}
 		}	
 	}
