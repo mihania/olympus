7654401f5460353443934937b5da48e08226d53e
OlegSmac <68604410+OlegSmac@users.noreply.github.com>
2022-02-02 17:13:01 +0200

    062

--- computer_science/062_Text/OlegSmac.cpp
+++ computer_science/062_Text/OlegSmac.cpp
@@ -12,42 +12,42 @@ int main() {
 	ofstream out("res.txt");
 	vector<string> words;
 	while (!in.eof()) {
-        string str;
-        in >> str;
-        if (str.size() != 0) {
-            words.push_back(str);
-        }
+		string str;
+		in >> str;
+		if (str.size() != 0) {
+		    words.push_back(str);
+		}
 	}
 	vector<int> numWords (words.size(), 1);
 	vector<vector<int>> resWords (words.size(), vector<int>{-1});
 	for (int i = 0; i < resWords.size(); i++) {
-        resWords[i][0] = i;
+        	resWords[i][0] = i;
 	}
 	//solution
 	int res = 0;
 	for (int i = 0; i < words.size(); i++) {
-        for (int j = 0; j < i; j++) {
-            if (words[j][words[j].size() - 1] == words[i][0]) {
-                if (numWords[j] + 1 > numWords[i]) {
-                    resWords[i].swap(resWords[j]);
-                    resWords[i].push_back(i);
+		for (int j = 0; j < i; j++) {
+		    if (words[j][words[j].size() - 1] == words[i][0]) {
+			if (numWords[j] + 1 > numWords[i]) {
+			    resWords[i].swap(resWords[j]);
+			    resWords[i].push_back(i);
 
-                }
-                numWords[i] = max(numWords[i], numWords[j] + 1);
-            }
-        }
+			}
+			numWords[i] = max(numWords[i], numWords[j] + 1);
+		    }
+		}
 	}
 	//output
 	int max = 0;
 	for (int i = 0; i < numWords.size(); i++) {
-        if (numWords[i] > numWords[max]) {
-            max = i;
-        }
+		if (numWords[i] > numWords[max]) {
+		    max = i;
+		}
 	}
 	out << words.size() << endl;
 	out << words.size() - numWords[max] << endl;
 	for (int i = 0; i < resWords[max].size(); i++) {
-        out << words[resWords[max][i]] << endl;
+        	out << words[resWords[max][i]] << endl;
 	}
 
 	in.close();
