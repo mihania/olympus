9296910d72c9f19cc71002d7ea38595c93667946
Mike Smacinih <mihania@gmail.com>
2022-02-09 13:00:32 -0800

    061.1

--- computer_science/061_Course/TestGenerator.java
+++ computer_science/061_Course/TestGenerator.java
@@ -5,27 +5,29 @@ import java.util.stream.*;
 
 public class TestGenerator {
     private static int solve1(List<int[]> m, int K) {
-    	// dp[i][j] = max LIS of not more than j intersected intervals on [0, i]
+    	// dp[i][k] = max LIS on [0, i] if i-th interval is intersected k times with previous intervals.
 	Collections.sort(m, (a, b) -> (a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]));
 	int[][] dp = new int[m.size()][K + 1];
-	int maxLis = 0;
+	int res = 0;
 	for (int i = 0; i < dp.length; i++) {
-		dp[i][0] = 1;
-		for (int k = 0; k <= K; k++) {
-			for (int j = 0; j < i; j++) {
-				int d = m.get(j)[1] < m.get(i)[0] ? 0 : 1;
+		dp[i][1] = 1;
+		for (int j = 0; j < i; j++) {
+			int d = m.get(j)[1] <= m.get(i)[0] ? 0 : 1;
+			for (int k = 1; k <= K; k++) {
 				if (d == 0) {
-					dp[i][0] = Math.max(dp[i][0], dp[j][k] + 1);
-				} else if (d + k <= K) {
-					dp[i][d + k] = Math.max(dp[i][d + k], dp[j][k] + 1);
+					dp[i][1] = Math.max(dp[i][1], dp[j][k] + 1);
+				} else if (k - 1 >= 0) {
+					dp[i][k] = Math.max(dp[i][k], dp[j][k - 1] + 1);
 				}
-			}
 
-			maxLis = Math.max(maxLis, dp[i][k]);
+				res = Math.max(res, dp[i][k]);
+				// System.out.printf("i=%d j=%d k=%d res=%d dp[i][k]=%d\n", i, j, k, res, dp[i][k]);
+			}
 		}
 	}
-	
-	return  maxLis;
+
+	// System.out.printf("\n");	
+	return res;
     }
     
     private static int solve2(List<int[]> m, int k) {
@@ -48,8 +50,6 @@ public class TestGenerator {
 		} else {
 			j = mid - 1;
 		}
-
-		// System.out.printf("i=%d j=%d mid=%d val=%b\n", i, j, mid, val);
 	}
 	
     	return i;
@@ -59,9 +59,8 @@ public class TestGenerator {
     	return m[1] - m[0] < duration ? m[1] : m[0] + duration;
     }
 
-    // returns false if m k more than k intersected intervals with  duration.
+    // returns true if intervals can be served with k room if all intervals will have duration duration, othwerwise false.
     private static boolean canServeWithKRooms(List<int[]> m, int duration, int k) {
-	// System.out.printf("can duration=%d k=%d\n", duration, k);
 	for (int i = 0; i < m.size(); ) {
 		int max = getEnd(m.get(i), duration);
 		int j = i;
@@ -69,12 +68,9 @@ public class TestGenerator {
 			max = Math.max(max, getEnd(m.get(j), duration));
 		}
 
-		// System.out.printf("\t duration=%d i=%d j=%d max=%d\n", duration, i, j, max);
-		
 		if (j - i > k) {
 			return false;
 		}
-
 
 		i = j == i ? i + 1 : j;
 	}
@@ -100,7 +96,7 @@ public class TestGenerator {
             testIn.append(String.valueOf(tests.size()));
             testIn.newLine();
             for (int i = 0; i < tests.size(); i++) {
-                Test test = tests.get(i);
+		Test test = tests.get(i);
                 solve(test);
 		testIn.append(String.valueOf(test.c));
 		testIn.newLine();
@@ -147,6 +143,15 @@ public class TestGenerator {
 
 	result.add(t2);
 	
+        Test t3 = new Test();
+	int[][] t3m = {{1, 3}, {1, 3}, {1, 3}, {1, 3}};
+	for (int[] k : t3m) {
+		t3.m.add(k);
+	}
+	t3.k = 2;
+	t3.c = 1;
+	result.add(t3);
+
 	for (int i = 0; i < T; i++) {
             
 	    Test test = new Test();
