0626e56a095ee50632163787123db6941b67fb5e
OlegSmac <68604410+OlegSmac@users.noreply.github.com>
2022-02-09 21:42:39 +0200

    062

--- computer_science/062_Text/OlegSmac.cpp
+++ computer_science/062_Text/OlegSmac.cpp
@@ -4,6 +4,7 @@
 #include<math.h>
 #include<string>
 #include<algorithm>
+#include<unordered_map>
 
 using namespace std;
 
@@ -12,42 +13,45 @@ int main() {
 	ofstream out("res.txt");
 	vector<string> words;
 	while (!in.eof()) {
-		string str;
-		in >> str;
-		if (str.size() != 0) {
-		    words.push_back(str);
-		}
+        string str;
+        in >> str;
+        if (str.size() != 0) {
+            words.push_back(str);
+        }
 	}
-	vector<int> numWords (words.size(), 1);
-	vector<vector<int>> resWords (words.size(), vector<int>{-1});
-	for (int i = 0; i < resWords.size(); i++) {
-        	resWords[i][0] = i;
-	}
-	//solution
-	int res = 0;
-	for (int i = 0; i < words.size(); i++) {
-		for (int j = 0; j < i; j++) {
-		    if (words[j][words[j].size() - 1] == words[i][0]) {
-			if (numWords[j] + 1 > numWords[i]) {
-			    resWords[i].swap(resWords[j]);
-			    resWords[i].push_back(i);
+	//dp[i] - количество слов в подпослделовательности, начинающейся с этого слова
+	vector<int> dp (words.size(), 1);
+	//resWords[i] - индекс следующего слова после words[i]
+	vector<int> resWords (words.size(), -1);
+	//let_idx - буква и её индекс, с которого нужно начинать подпоследовательность
+	unordered_map<char, int> let_idx;
 
-			}
-			numWords[i] = max(numWords[i], numWords[j] + 1);
-		    }
-		}
+	//solution
+	int maxIdx = 0;
+	for (int i = words.size() - 1; i >= 0; i--) {
+        char beginWord = words[i][0];
+        char endWord = words[i][words[i].size() - 1];
+        if (let_idx.count(beginWord) == 0) {
+            let_idx[beginWord] = i;
+        }
+        if (let_idx.count(endWord) != 0) {
+            if (dp[i] < dp[let_idx[endWord]] + 1) {
+                dp[i] = dp[let_idx[endWord]] + 1;
+                resWords[i] = let_idx[endWord];
+                let_idx[beginWord] = i;
+            }
+            if (dp[i] > dp[maxIdx]) {
+                maxIdx = i;
+            }
+        }
 	}
+
 	//output
-	int max = 0;
-	for (int i = 0; i < numWords.size(); i++) {
-		if (numWords[i] > numWords[max]) {
-		    max = i;
-		}
-	}
 	out << words.size() << endl;
-	out << words.size() - numWords[max] << endl;
-	for (int i = 0; i < resWords[max].size(); i++) {
-        	out << words[resWords[max][i]] << endl;
+	out << words.size() - dp[maxIdx] << endl;
+	while (maxIdx != -1) {
+        out << words[maxIdx] << endl;
+        maxIdx = resWords[maxIdx];
 	}
 
 	in.close();
