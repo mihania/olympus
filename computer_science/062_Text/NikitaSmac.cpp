5cf0f625849fe1c4cd567b942b9d51151f4d93d7
Nikitasm <68600537+NikitaSmac@users.noreply.github.com>
2022-02-11 17:46:41 +0200

    Update NikitaSmac.cpp

--- computer_science/062_Text/NikitaSmac.cpp
+++ computer_science/062_Text/NikitaSmac.cpp
@@ -20,14 +20,18 @@ int main(){
     vector<vector<string>> res_words(all_words.size(), vector<string>(0, ""));
     int ans_ind = 0;
     int ans = 0;
-    for(int i = 1; i < all_words.size(); i++) {
-        for(int j = 0; j < i; j++){
+    for(int i = 0; i < all_words.size(); i++) {
+        for(int j = i; j >= 0; j--){
             int s = all_words[j].size();
             if (dp[i] < dp[j] + 1 && all_words[j][s-1] == all_words[i][0]){
                 dp[i] = dp[j] + 1;
-                res_words[i].push_back(all_words[j]);
+                for(int k = 0; k < res_words[j].size(); k++){
+                    res_words[i].push_back(res_words[j][k]);
+                }
+                break;
             }
         }
+        res_words[i].push_back(all_words[i]);
         if(ans < dp[i]){
             ans = dp[i];
             ans_ind = i;
@@ -35,12 +39,12 @@ int main(){
     }
 
     out << all_words.size() << endl;
-    out << all_words.size() - ans<< endl;
+    out << all_words.size() - ans << endl;
 
     for(int j = 0; j < res_words[ans_ind].size(); j++){
         out << res_words[ans_ind][j] << endl;
     }
+
     in.close();
     out.close();
 }
-
